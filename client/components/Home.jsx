82f43e8841bc70d085dd6834b4e101a68b9b0e3a
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-16 18:28:44 +1200

    Initialising homepage

---
+++ client/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>We exquisitely create customised cakes for special occasions!</h2>
+      <img
+        src="https://github.com/sherelynn-baguioso/my-photos/blob/6794d8ab4d141c7fcc28a60bc38c759e44ffea7c/gary-scott-GD8EANdZu28-unsplash.jpg?raw=true"
+        // width={700}
+        // height={500}
+        alt="close up cake on a plate"
+        className="homepage-image"
+      />
+    </div>
+  )
+}
+
+export default Home
