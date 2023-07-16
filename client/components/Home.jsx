fb4debdcda60f6c80cd4f2646e7addf5e6a9620f
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-16 20:15:09 +1200

    Remove initial width and height of homepage image

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,8 +6,6 @@ const Home = () => {
       <h2>We exquisitely create customised cakes for special occasions!</h2>
       <img
         src="https://github.com/sherelynn-baguioso/my-photos/blob/6794d8ab4d141c7fcc28a60bc38c759e44ffea7c/gary-scott-GD8EANdZu28-unsplash.jpg?raw=true"
-        // width={700}
-        // height={500}
         alt="close up cake on a plate"
         className="homepage-image"
       />
