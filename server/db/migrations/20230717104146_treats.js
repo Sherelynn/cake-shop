dcef3f3688a55bcad02ab214cb54946692d80ceb
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-17 22:49:44 +1200

    Add database migrations and seeds for treats

---
+++ server/db/migrations/20230717104146_treats.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('treats', (table) => {
+    table.increments()
+    table.string('treats')
+    table.integer('price')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('treats')
+}
