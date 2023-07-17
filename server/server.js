ec98573feb77c2c7fa594bb27bd077ae18ea0440
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-17 22:04:33 +1200

    Add endpoint for cake types

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const flavours = require('./routes/flavours')
+const cakeTypes = require('./routes/cakeTypes')
 
 const server = express()
 
@@ -9,6 +10,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/menu/flavours', flavours)
+server.use('/api/v1/menu/cakeTypes', cakeTypes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
