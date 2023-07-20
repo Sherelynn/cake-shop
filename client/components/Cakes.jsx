a5cb08b79c90c4d0743e841d009c8cf34c9c944f
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-20 17:18:41 +1200

    Add images and styling for the 'Cakes' page

--- client/components/Cakes.jsx
+++ client/components/Cakes.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
 
 const Cakes = () => {
-  return <h1>Cakes Page</h1>
+  return (
+    <>
+      <h1>Featured Cakes</h1>
+      <section id="cake-images">
+        <div className="cake-image-tile">
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-4.jpg?raw=true"
+            alt="adventure cake"
+          />
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-5.jpg?raw=true"
+            alt="basketball cake"
+          />
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-6.jpg?raw=true"
+            alt="pirate cake"
+          />
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-7.jpg?raw=true"
+            alt="bike rider cake"
+          />
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-8.jpg?raw=true"
+            alt="liquor cake"
+          />
+          <img
+            className="cake-image"
+            src="https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-9.jpg?raw=true"
+            alt="blue and gold cake"
+          />
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Cakes
