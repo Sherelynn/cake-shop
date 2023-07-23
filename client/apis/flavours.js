1b3141f828720d81a872612ad0b5eacf35f2c172
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-24 11:58:35 +1200

    Add getFllavours function to fetch flavours from the server

---
+++ client/apis/flavours.js
@@ -0,0 +1,14 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1/menu/flavours/'
+
+const getFlavours = async () => {
+  try {
+    const res = await request.get(rootUrl)
+    return res.body
+  } catch (err) {
+    return Promise.reject(`Error fetching flavours: ${err.message}` )
+  }
+}
+
+export { getFlavours }
