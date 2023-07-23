1e3936f3ac30ec9eda460ba35869115f3c025dbc
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-24 11:49:51 +1200

    Move cake images data in a separate file

---
+++ client/src/cakeImagesData.js
@@ -0,0 +1,34 @@
+const cakeImagesData = [
+  {
+    id: 1,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-4.jpg?raw=true',
+    alt: 'Adventure Cake',
+  },
+  {
+    id: 2,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-5.jpg?raw=true',
+    alt: 'Basketball Cake',
+  },
+  {
+    id: 3,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-6.jpg?raw=true',
+    alt: 'Pirate Cake',
+  },
+  {
+    id: 4,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-7.jpg?raw=true',
+    alt: 'Bike Rider Cake',
+  },
+  {
+    id: 5,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-8.jpg?raw=true',
+    alt: 'Liquor Cake',
+  },
+  {
+    id: 6,
+    src: 'https://github.com/sherelynn-baguioso/my-photos/blob/main/cake-9.jpg?raw=true',
+    alt: 'Blue and Gold Cake',
+  },
+]
+
+export default cakeImagesData
