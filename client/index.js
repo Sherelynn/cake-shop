c2b84c3fab8dd9cb3d8977ac79e7f4e87bd85df3
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-24 12:05:37 +1200

    Render React App with Redux store and React Router

--- client/index.js
+++ client/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import store from './store'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import App from './components/App'
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   ReactDOM.render(<App />, document.getElementById('app'))
-// })
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    // <Provider store={store}>
+    <Provider store={store}>
       <Router>
         <App />
-      </Router>,
-    // </Provider>,
+      </Router>
+    </Provider>,
     document.getElementById('app'),
   )
 })
