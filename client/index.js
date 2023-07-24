04e91cbcf141fc3b0a62b01333dd10d775ae59ad
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-24 12:27:32 +1200

    Change ReactDOM with createRoot as it is no longer supported

--- client/index.js
+++ client/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client' // Import createRoot from 'react-dom/client' instead of 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
 import store from './store'
 import App from './components/App'
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+const rootElement = document.getElementById('app')
+const root = createRoot(rootElement)
+
+root.render(
+  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>,
-    document.getElementById('app'),
-  )
-})
+    </Provider>
+  </React.StrictMode>,
+)
