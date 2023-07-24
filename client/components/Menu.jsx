19d047d9cf0fbc8dea378c3777f8a311c08e9289
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-07-24 12:32:30 +1200

    Check if flavours are fetched successfully

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchFlavours } from '../actions/flavours'
 
 const Menu = () => {
-  return <h1>Menu Page</h1>
+  const dispatch = useDispatch()
+  const flavours = useSelector((state) => state.flavours.data)
+
+  useEffect(() => {
+    dispatch(fetchFlavours())
+  }, [])
+
+  return (
+    <div>
+      <h1>Flavours</h1>
+      <ul>
+        {flavours.map((flavour) => (
+          <li key={flavour.id}>{flavour.flavours}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Menu
