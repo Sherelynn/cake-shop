d9de519445ce3e91d1019970f11e1b752a8132f6
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-03 22:12:14 +1200

    Edit Menu Component to initially display table of flavours, types of cake, and treats

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,23 +1,75 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchFlavours } from '../actions/flavours'
+import { fetchCakeTypes } from '../actions/cakeTypes'
+import { fetchTreats } from '../actions/treats'
 
 const Menu = () => {
   const dispatch = useDispatch()
   const flavours = useSelector((state) => state.flavours.data)
+  const cakeTypes = useSelector((state) => state.cakeTypes.data)
+  const treats = useSelector((state) => state.treats.data)
 
   useEffect(() => {
     dispatch(fetchFlavours())
   }, [])
 
+  useEffect(() => {
+    dispatch(fetchCakeTypes())
+  }, [])
+
+  useEffect(() => {
+    dispatch(fetchTreats())
+  }, [])
+
   return (
-    <div>
-      <h1>Flavours</h1>
-      <ul>
-        {flavours.map((flavour) => (
-          <li key={flavour.id}>{flavour.flavours}</li>
-        ))}
-      </ul>
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Flavours</th>
+          </tr>
+        </thead>
+        <tbody>
+          {flavours.map((flavour) => (
+            <tr key={flavour.id}>
+              <td>{flavour.flavours}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Types of Cake</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cakeTypes.map((cakeType) => (
+            <tr key={cakeType.id}>
+              <td>{cakeType.cakeTypes}</td>
+              <td>${cakeType.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Treats</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {treats.map((treat) => (
+            <tr key={treat.id}>
+              <td>{treat.treats}</td>
+              <td>${treat.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
