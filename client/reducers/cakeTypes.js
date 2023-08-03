7b51883d7b8589976ecbb05e656f36f76dd91887
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-03 22:02:47 +1200

    Add initial cakeTypes reducer and action types

---
+++ client/reducers/cakeTypes.js
@@ -0,0 +1,26 @@
+import {
+  CAKETYPES_PENDING,
+  CAKETYPES_REJECTED,
+  GET_CAKETYPES_FULFILLED,
+} from '../actions/cakeTypes'
+
+const initialState = {
+  data: [],
+  error: null,
+  loading: false,
+}
+
+const cakeTypes = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case CAKETYPES_PENDING:
+      return { ...state, loading: true }
+    case CAKETYPES_REJECTED:
+      return { ...state, error: payload }
+    case GET_CAKETYPES_FULFILLED:
+      return { ...state, data: payload }
+    default:
+      return state
+  }
+}
+
+export default cakeTypes
