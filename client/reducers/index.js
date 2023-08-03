789896730a21326757e5cc1d2d00d8f0ed432ef3
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-03 22:09:24 +1200

    Add treats in combineReducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import flavours from './flavours'
 import cakeTypes from './cakeTypes'
+import treats from './treats'
 
 export default combineReducers({
   flavours,
   cakeTypes,
+  treats,
 })
