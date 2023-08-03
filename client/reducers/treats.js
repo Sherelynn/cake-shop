584a60c8beec7dcbc8e7c3d2b760b240577b1a2b
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-03 22:08:53 +1200

    Add initial treats reducer and action types

---
+++ client/reducers/treats.js
@@ -0,0 +1,26 @@
+import {
+  TREATS_PENDING,
+  TREATS_REJECTED,
+  GET_TREATS_FULFILLED,
+} from '../actions/treats'
+
+const initialState = {
+  data: [],
+  error: null,
+  loading: false,
+}
+
+const treats = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case TREATS_PENDING:
+      return { ...state, loading: true }
+    case TREATS_REJECTED:
+      return { ...state, error: payload }
+    case GET_TREATS_FULFILLED:
+      return { ...state, data: payload }
+    default:
+      return state
+  }
+}
+
+export default treats
