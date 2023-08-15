14e8d70d53e916b5fd91d26d3b9bd7e82bca0ee7
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-15 14:15:39 +1200

    Add isLoading and error from Auth0 to Login Page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 import AdminButton from './AdminButton'
+import UserProfile from './UserProfile'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Login = () => {
+  const { isLoading, error } = useAuth0()
   return (
     <div>
       <h1>Login Page</h1>
-      <LoginButton />
-      <LogoutButton />
-      <AdminButton />
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <div className="loader"></div>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <UserProfile />
+          <LogoutButton />
+          <AdminButton />
+        </>
+      )}
     </div>
   )
 }
