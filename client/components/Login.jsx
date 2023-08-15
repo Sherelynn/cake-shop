027e05eb213f51b604c58d98df3a6b9065c9ae0b
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-15 17:08:33 +1200

    Modify Login Page UI and add User Profile

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -8,18 +8,28 @@ import { useAuth0 } from '@auth0/auth0-react'
 const Login = () => {
   const { isLoading, error } = useAuth0()
   return (
-    <div>
+    <div className="login-page">
       <h1>Login Page</h1>
-      {error && <p>Authentication Error</p>}
-      {!error && isLoading && <div className="loader"></div>}
-      {!error && !isLoading && (
-        <>
-          <LoginButton />
-          <UserProfile />
-          <LogoutButton />
-          <AdminButton />
-        </>
-      )}
+      <div>
+        {error && <p>Authentication Error</p>}
+        {!error && isLoading && <div className="loader"></div>}
+        {!error && !isLoading && (
+          <>
+            <LoginButton />
+            <div className="logout-admin-buttons">
+              <div>
+                <LogoutButton />
+              </div>
+              <div>
+                <AdminButton />
+              </div>
+            </div>
+            <div>
+              <UserProfile />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
