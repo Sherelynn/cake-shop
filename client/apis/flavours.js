7952e79b6cc16f3d946b82cb9e7d1cd1418635e0
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-17 01:20:11 +1200

    Implement client API addFlavour function

--- client/apis/flavours.js
+++ client/apis/flavours.js
@@ -2,13 +2,26 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1/menu/flavours/'
 
+const handleError = (err) => {
+  return Promise.reject(`Error: ${err.message}`)
+}
+
 const getFlavours = async () => {
   try {
     const res = await request.get(rootUrl)
     return res.body
   } catch (err) {
-    return Promise.reject(`Error fetching flavours: ${err.message}`)
+    return handleError(err)
   }
 }
 
-export { getFlavours }
+const addFlavour = async (newFlavour) => {
+  try {
+    const res = await request.post(rootUrl).send({ flavours: newFlavour })
+    return res.body
+  } catch (err) {
+    return handleError(err)
+  }
+}
+
+export { getFlavours, addFlavour }
