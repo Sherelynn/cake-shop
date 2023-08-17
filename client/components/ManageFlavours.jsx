c5a4554fc333e650bd020c9d0cc3fe28953039cf
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-17 12:33:10 +1200

    Update ManageFlavour component for updating a flavour

--- client/components/ManageFlavours.jsx
+++ client/components/ManageFlavours.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchFlavours, postFlavour } from '../actions/flavours'
+import { fetchFlavours, postFlavour, patchFlavour } from '../actions/flavours'
 
 const ManageFlavours = () => {
   const dispatch = useDispatch()
   const flavours = useSelector((state) => state.flavours.data)
   const [newFlavour, setNewFlavour] = useState('')
+  const [flavourId, setFlavourId] = useState('')
+  const [updatedFlavour, setUpdatedFlavour] = useState('')
 
   useEffect(() => {
     dispatch(fetchFlavours())
   }, [])
+
+  // Adding a new flavour
 
   const handleAddNewFlavour = (event) => {
     setNewFlavour(event.target.value)
@@ -19,6 +23,23 @@ const ManageFlavours = () => {
     event.preventDefault()
     dispatch(postFlavour(newFlavour))
     setNewFlavour('')
+  }
+
+  // Updating a flavour
+
+  const handleFlavourSelection = (event) => {
+    setFlavourId(event.target.value)
+  }
+
+  const handleUpdatedFlavour = (event) => {
+    setUpdatedFlavour(event.target.value)
+  }
+
+  const handleSubmitUpdatedFlavour = (event) => {
+    event.preventDefault()
+    dispatch(patchFlavour(flavourId, updatedFlavour))
+    setUpdatedFlavour('')
+    setFlavourId('')
   }
 
   return (
@@ -33,10 +54,14 @@ const ManageFlavours = () => {
           <tbody>
             <tr>
               <td>
-                <select name="Selections">
+                <select
+                  name="Selections"
+                  value={flavourId}
+                  onChange={handleFlavourSelection}
+                >
                   <option value="">Selections:</option>
                   {flavours.map((flavour) => (
-                    <option key={flavour.id} value={flavour.flavours}>
+                    <option key={flavour.id} value={flavour.id}>
                       {flavour.flavours}
                     </option>
                   ))}
@@ -51,8 +76,29 @@ const ManageFlavours = () => {
                   value={newFlavour}
                   onChange={handleAddNewFlavour}
                 />
-                <button className="input-button" type="submit" onClick={handleSubmitNewFlavour}>
+                <button
+                  className="input-button"
+                  type="submit"
+                  onClick={handleSubmitNewFlavour}
+                >
                   Add
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  placeholder="Update a flavour:"
+                  value={updatedFlavour}
+                  onChange={handleUpdatedFlavour}
+                />
+                <button
+                  className="input-button"
+                  type="submit"
+                  onClick={handleSubmitUpdatedFlavour}
+                >
+                  Update
                 </button>
               </td>
             </tr>
