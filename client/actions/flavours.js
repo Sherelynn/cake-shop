8cbe1ec2bb75e41dae8b75e95cccb5c78ee9d83d
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-17 12:26:37 +1200

    Add Redux actions for patching or updating a flavour

--- client/actions/flavours.js
+++ client/actions/flavours.js
@@ -1,10 +1,11 @@
-import { getFlavours, addFlavour } from '../apis/flavours'
+import { getFlavours, addFlavour, updateFlavour } from '../apis/flavours'
 
 // Action types
 export const FLAVOURS_PENDING = 'FLAVOURS_PENDING'
 export const FLAVOURS_REJECTED = 'FLAVOURS_REJECTED'
 export const GET_FLAVOURS_FULFILLED = 'GET_FLAVOURS_FULFILLED'
 export const ADD_FLAVOUR_FULFILLED = 'ADD_FLAVOUR_FULFILLED'
+export const UPDATE_FLAVOUR_FULFILLED = 'UPDATE_FLAVOUR_FULFILLED'
 
 // Action creators
 export const flavoursPending = () => ({
@@ -26,6 +27,11 @@ export const addFlavourFulfilled = (newFlavour) => ({
   payload: newFlavour,
 })
 
+export const updateFlavourFulfilled = (flavourUpdated) => ({
+  type: UPDATE_FLAVOUR_FULFILLED,
+  payload: flavourUpdated,
+})
+
 // Async thunk action creators using async/await
 export const fetchFlavours = () => async (dispatch) => {
   dispatch(flavoursPending())
@@ -44,5 +50,15 @@ export const postFlavour = (newFlavour) => async (dispatch) => {
     dispatch(addFlavourFulfilled(flavour))
   } catch (err) {
     dispatch(flavoursRejected(`Error adding flavour: ${err.message}`))
+  }
+}
+
+export const patchFlavour = (flavourId, updatedFlavour) => async (dispatch) => {
+  dispatch(flavoursPending())
+  try {
+    const flavourUpdated = await updateFlavour(flavourId, updatedFlavour)
+    dispatch(updateFlavourFulfilled(flavourUpdated))
+  } catch (err) {
+    dispatch(flavoursRejected(`Error updating flavour: ${err.message}`))
   }
 }
