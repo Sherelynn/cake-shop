4107dab91ac6ed2a9ce3e13df27b8217f27cdefe
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-20 03:10:48 +1200

    Import ManageCakeTypes component and implement table styling

--- client/components/Admin.jsx
+++ client/components/Admin.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ManageFlavours from './ManageFlavours'
+import ManageCakeTypes from './ManageCakeTypes'
 
 const Admin = () => {
   return (
     <div>
       <h1>Admin Page</h1>
-      <div>
-        <ManageFlavours />
+      <div className="table-container">
+        <table className="table">
+          <ManageFlavours />
+        </table>
+        <table className="table">
+          <ManageCakeTypes />
+        </table>
       </div>
     </div>
   )
