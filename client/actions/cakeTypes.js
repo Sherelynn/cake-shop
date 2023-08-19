da9d2b1e6ab2187ccdf6fa547399a325bbd283d1
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-20 03:05:31 +1200

    Add Redux actions for adding a cake

--- client/actions/cakeTypes.js
+++ client/actions/cakeTypes.js
@@ -1,9 +1,10 @@
-import { getCakeTypes } from '../apis/cakeTypes'
+import { getCakeTypes, addCakeType } from '../apis/cakeTypes'
 
 // Action types
 export const CAKETYPES_PENDING = 'CAKETYPES_PENDING'
 export const CAKETYPES_REJECTED = 'CAKETYPES_REJECTED'
 export const GET_CAKETYPES_FULFILLED = 'GET_CAKETYPES_FULFILLED'
+export const ADD_CAKETYPE_FULFILLED = 'ADD_CAKETYPE_FULFILLED'
 
 // Action creators
 export const cakeTypesPending = () => ({
@@ -20,6 +21,11 @@ export const getCakeTypesFulfilled = (cakeTypes) => ({
   payload: cakeTypes,
 })
 
+export const addCakeTypeFulfilled = (newCakeAndPrice) => ({
+  type: ADD_CAKETYPE_FULFILLED,
+  payload: newCakeAndPrice,
+})
+
 // Async thunk action creators using async/await
 export const fetchCakeTypes = () => async (dispatch) => {
   dispatch(cakeTypesPending())
@@ -28,5 +34,15 @@ export const fetchCakeTypes = () => async (dispatch) => {
     dispatch(getCakeTypesFulfilled(cakeTypes))
   } catch (err) {
     dispatch(cakeTypesRejected(`Error fetching cake types: ${err.message}`))
+  }
+}
+
+export const postCakeType = (newCake, newPrice) => async (dispatch) => {
+  dispatch(cakeTypesPending())
+  try {
+    const newCakeAndPrice = await addCakeType(newCake, newPrice)
+    dispatch(addCakeTypeFulfilled(newCakeAndPrice))
+  } catch (err) {
+    dispatch(cakeTypesRejected(`Error adding cake: ${err.message}`))
   }
 }
