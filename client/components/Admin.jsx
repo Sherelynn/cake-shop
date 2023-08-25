9d3cd07d90ea1ccbbf50b8ecad7a4b6e7d90cfb5
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-25 22:19:14 +1200

    Protect /admin route if not authenticated

--- client/components/Admin.jsx
+++ client/components/Admin.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import ManageFlavours from './ManageFlavours'
 import ManageCakeTypes from './ManageCakeTypes'
 import ManageTreats from './ManageTreats'
+import LoginButton from './LoginButton'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Admin = () => {
+  const { isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return <div className="loader"></div>
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <h1>Access Denied</h1>
+        <LoginButton />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Admin Page</h1>
