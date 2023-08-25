51359839221969b421d24ff599cdddd8aaf82b18
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-25 22:49:42 +1200

    Modify Menu component for styling

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -19,53 +19,56 @@ const Menu = () => {
 
   return (
     <>
-      <div className="table-container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Flavours</th>
-            </tr>
-          </thead>
-          <tbody>
-            {flavours.map((flavour) => (
-              <tr key={flavour.id}>
-                <td>{flavour.flavours}</td>
+      <div id="menu">
+        <h1>Menu</h1>
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Flavours</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Types of Cake</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cakeTypes.map((cakeType) => (
-              <tr key={cakeType.id}>
-                <td>{cakeType.cakeTypes}</td>
-                <td>${cakeType.price}</td>
+            </thead>
+            <tbody>
+              {flavours.map((flavour) => (
+                <tr key={flavour.id}>
+                  <td>{flavour.flavours}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Types of Cake</th>
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Treats</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {treats.map((treat) => (
-              <tr key={treat.id}>
-                <td>{treat.treats}</td>
-                <td>${treat.price}</td>
+            </thead>
+            <tbody>
+              {cakeTypes.map((cakeType) => (
+                <tr key={cakeType.id}>
+                  <td>{cakeType.cakeTypes}</td>
+                  <td>${cakeType.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Treats</th>
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {treats.map((treat) => (
+                <tr key={treat.id}>
+                  <td>{treat.treats}</td>
+                  <td>${treat.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <div id="cart">
         {/* Display Cart */}
