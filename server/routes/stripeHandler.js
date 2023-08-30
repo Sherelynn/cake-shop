362b2a7f550031dabddc25bdc27ed3d71ad3d37e
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-08-30 22:36:20 +1200

    Create stripe handler in server - Initial

---
+++ server/routes/stripeHandler.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST)
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+)
+
+app.options('*', cors())
+
+app.post('/payment', cors(), async (req, res) => {
+  let { amount, id } = req.body
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: 'NZD',
+      description: 'The Flourist NZ',
+      payment_method: id,
+      confirm: true,
+    })
+    console.log('Payment', payment)
+    res.json({
+      message: 'Payment successful',
+      success: true,
+    })
+  } catch (error) {
+    console.log('Error', error)
+    res.json({
+      message: 'Payment failed',
+      success: false,
+    })
+  }
+})
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Sever is listening on port 4000')
+})
