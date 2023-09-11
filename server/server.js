922b28913b3686b597fef17967a684d62e778a74
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-09-11 15:33:48 +1200

    Add endpoint for Stripe in server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const flavours = require('./routes/flavours')
 const cakeTypes = require('./routes/cakeTypes')
 const treats = require('./routes/treats')
+const stripeHandler = require('./routes/stripeHandler')
 
 const server = express()
 
@@ -13,6 +14,7 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use('/api/v1/menu/flavours', flavours)
 server.use('/api/v1/menu/cakeTypes', cakeTypes)
 server.use('/api/v1/menu/treats', treats)
+server.use('/api/v1/payment', stripeHandler)
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
