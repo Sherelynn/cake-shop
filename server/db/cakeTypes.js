5db7f64b22a7bd2e65c959c0fb47e74202cdc1ab
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-09-12 15:39:12 +1200

    Modify addCakeType db function to avoid duplicates when adding new cake type

--- server/db/cakeTypes.js
+++ server/db/cakeTypes.js
@@ -9,6 +9,16 @@ const getCakeTypeById = async (id, db = connection) =>
     .first()
 
 const addCakeType = async (newCakeType, db = connection) => {
+  // Check if a cake type with the same name already exists
+  const existingCakeType = await db('cake_types')
+    .where('cakeTypes', newCakeType.cakeTypes)
+    .first()
+
+  if (existingCakeType) {
+    throw new Error('Cake type with this name already exists.')
+  }
+
+  // If it doesn't exist insert the new cake type
   const [id] = await db('cake_types').insert(newCakeType)
   return id
 }
