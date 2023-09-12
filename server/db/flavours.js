47a6012a31104c0f6a6c6c88280f2d6acea5df4f
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-09-12 16:01:14 +1200

    Modify addFlavour db function to cavoid duplicates when adding new flavour

--- server/db/flavours.js
+++ server/db/flavours.js
@@ -6,6 +6,16 @@ const getFlavourById = async (id, db = connection) =>
   await db('flavours').where({ id }).select('id', 'flavours').first()
 
 const addFlavour = async (newFlavour, db = connection) => {
+  // Check if a flavour with the same name already exists
+  const existingFlavour = await db('flavours')
+    .where('flavours', newFlavour.flavours)
+    .first()
+
+  if (existingFlavour) {
+    throw new Error('Flavour with this name already exists.')
+  }
+
+  // If it doesn't exist insert the new flavour
   const [id] = await db('flavours').insert(newFlavour)
   return id
 }
