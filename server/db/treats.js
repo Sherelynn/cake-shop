7a5c7c5e3c09d569a8bdb935f43787986b178551
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-09-12 16:06:46 +1200

    Modify addTreat db function to avoid duplicates when adding new treat

--- server/db/treats.js
+++ server/db/treats.js
@@ -6,6 +6,16 @@ const getTreatById = async (id, db = connection) =>
   await db('treats').where({ id }).select('id', 'treats', 'price').first()
 
 const addTreat = async (newTreat, db = connection) => {
+  // Check if a treat with the same name already exists
+  const existingTreat = await db('treats')
+    .where('treats', newTreat.treats)
+    .first()
+
+  if (existingTreat) {
+    throw new Error('Treat with this name already exists.')
+  }
+
+  // If it doesn't exist insert the new treat
   const [id] = await db('treats').insert(newTreat)
   return id
 }
